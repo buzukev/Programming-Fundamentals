fc3cbe38a4b56e6e02f41438e494529f9bbbba7b
buzukev <buzukev@gmail.com>
2016-10-12 21:50:29 +0300

    Add BookLibrary, BookLibrary Modifications and AndreyAndBiliard

---
+++ 06.Objects and Classes -  Exercises/AndreyAndBilliard/AndreyAndBilliard.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+public class AndreyAndBilliard
+{
+    public static void Main()
+    {
+        Dictionary<string, decimal> productsPrice = new Dictionary<string, decimal>();
+        int productsCount = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < productsCount; i++)
+        {
+            string[] input = Console.ReadLine().Split('-');
+            string productName = input[0];
+            decimal productPrice = decimal.Parse(input[1]);
+
+            if (!productsPrice.ContainsKey(productName))
+            {
+                productsPrice.Add(productName, productPrice);
+            }
+            productsPrice[productName] = productPrice;
+        }
+
+        string inputLine = Console.ReadLine();
+
+        List<Customer> customers = new List<Customer>();
+
+        while (!inputLine.Equals("end of clients"))
+        {
+            string[] customerInfo = inputLine.Split('-', ',');
+            string customerName = customerInfo[0];
+            string customerProduct = customerInfo[1];
+            int quantity = int.Parse(customerInfo[2]);
+
+            if (!productsPrice.ContainsKey(customerProduct))
+            {
+                inputLine = Console.ReadLine();
+                continue;
+            }
+
+            if (customers.Any(x => x.Name == customerName))
+            {
+                Customer customer = customers.First(x => x.Name == customerName);
+
+                if (!customer.OrdersPrice.ContainsKey(customerProduct))
+                {
+                    customer.OrdersPrice.Add(customerProduct, quantity);
+                }
+                else
+                {
+                    customer.OrdersPrice[customerProduct] += quantity;
+                }
+
+                customer.Bill += quantity * productsPrice[customerProduct];
+            }
+
+            else
+            {
+                Customer customer = new Customer();
+                customer.Name = customerName;
+                customer.OrdersPrice = new Dictionary<string, int>();
+                customer.OrdersPrice.Add(customerProduct, quantity);
+                customer.Bill += quantity * productsPrice[customerProduct];
+                customers.Add(customer);
+            }
+
+            inputLine = Console.ReadLine();
+        }
+
+        foreach (Customer customer in customers.OrderBy(x => x.Name))
+        {
+            Console.WriteLine(customer.Name);
+
+            foreach (KeyValuePair<string, int> cutomerOrderes in customer.OrdersPrice)
+            {
+                Console.WriteLine($"-- {cutomerOrderes.Key} - {cutomerOrderes.Value}");
+            }
+            Console.WriteLine($"Bill: {customer.Bill:f2}");
+        }
+        decimal totalBill = customers.Sum(x => x.Bill);
+        Console.WriteLine($"Total bill: {totalBill:f2}");
+    }
+    public class Customer
+    {
+        public string Name { get; set; }
+        public Dictionary<string, int> OrdersPrice { get; set; }
+        public decimal Bill { get; set; }
+    }
+}
---
+++ 06.Objects and Classes -  Exercises/BookLibraryModification/BookLibraryModification.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+public class Book
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string Publisher { get; set; }
+    public DateTime ReleaseDate { get; set; }
+    public string isbnNumber { get; set; }
+    public double Price { get; set; }
+}
+public class Library
+{
+    public string Name { get; set; }
+    public List<Book> Books { get; set; }
+}
+
+
+public class BookLibraryModification
+{
+    public static void Main()
+    {
+        Library Library = new Library();
+        Library.Books = new List<Book>();
+
+        int numberOfBooks = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < numberOfBooks; i++)
+        {
+            string[] input = Console.ReadLine().Split(' ').ToArray();
+            string title = input[0];
+            string author = input[1];
+            string publisher = input[2];
+            DateTime releaseDate = DateTime.ParseExact(input[3], "dd.MM.yyyy", CultureInfo.InvariantCulture);
+            string isbn = input[4];
+            double price = double.Parse(input[5]);
+            Book book = new Book();
+            book.Title = title;
+            book.Author = author;
+            book.Publisher = publisher;
+            book.ReleaseDate = releaseDate;
+            book.isbnNumber = isbn;
+            book.Price = price;
+            Library.Books.Add(book);
+        }
+
+        string inputDate = Console.ReadLine();
+        DateTime startDate = DateTime.ParseExact(inputDate, "dd.MM.yyyy", CultureInfo.InvariantCulture);
+
+        Dictionary<string, DateTime> filteredBook = new Dictionary<string, DateTime>();
+
+        for (int i = 0; i < Library.Books.Count; i++)
+        {
+            if (!filteredBook.ContainsKey(Library.Books[i].Title))
+            {
+                filteredBook.Add(Library.Books[i].Title, Library.Books[i].ReleaseDate);
+            }
+
+            else
+            {
+                filteredBook[Library.Books[i].Title] = Library.Books[i].ReleaseDate;
+            }
+        }
+        foreach (var book in filteredBook.Where(s => s.Value > startDate).OrderBy(x => x.Value).ThenBy(y => y.Key))
+        {
+            Console.WriteLine($"{book.Key} -> {book.Value.ToString("dd.MM.yyyy")}");
+        }
+    }
+}
