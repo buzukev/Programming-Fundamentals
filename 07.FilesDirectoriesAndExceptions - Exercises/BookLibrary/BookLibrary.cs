447b873fa75516bbab3a8ae25e63a7eba5892c9f
buzukev <buzukev@gmail.com>
2016-10-16 15:36:20 +0300

    Add new projects in Files-Directories And Exceptions - BookLibrary and BookLibraryModification

---
+++ 07.FilesDirectoriesAndExceptions - Exercises/BookLibrary/BookLibrary.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.IO;
+public class BookLibrary
+{
+    public static void Main()
+    {
+        string[] input = File.ReadAllLines("input.txt");
+
+        int booksCount = int.Parse(input[0]);
+
+        Library library = new Library("My Library");
+
+        for (int i = 0; i < booksCount; i++)
+        {
+            string inputLine = input[i+1];
+            string[] inputArgs = inputLine.Split(' ');
+            string title = inputArgs[0];
+            string author = inputArgs[1];
+            string publisher = inputArgs[2];
+            DateTime releaseDate = DateTime.ParseExact(inputArgs[3], "dd.MM.yyyy", CultureInfo.InvariantCulture);
+            string isbnNumber = inputArgs[4];
+            decimal price = decimal.Parse(inputArgs[5]);
+
+            Book book = new Book(title, author, publisher, releaseDate, isbnNumber, price);
+            library.Books.Add(book);
+        }
+
+        Dictionary<string, List<Book>> booksByAuthors = new Dictionary<string, List<Book>>();
+
+        foreach (Book book in library.Books)
+        {
+            if (!booksByAuthors.ContainsKey(book.Author))
+            {
+                booksByAuthors.Add(book.Author, new List<Book>());
+            }
+
+            booksByAuthors[book.Author].Add(book);
+        }
+
+        foreach (var author in booksByAuthors.OrderByDescending(x => x.Value.Select(y =>y.Price).Sum()).ThenBy(x => x.Key))
+        {
+            decimal totalPrice = author.Value.Select(x => x.Price).Sum();
+
+            File.AppendAllText("output.txt", string.Format($"{author.Key} -> {totalPrice}" + Environment.NewLine));
+        }
+
+    }
+}
+
+public class Book
+{
+    public Book(string title, string author, string publisher, DateTime releaseDate, string isbnNumber, decimal price)
+    {
+        this.Author = author;
+        this.Title = title;
+        this.Publisher = publisher;
+        this.ReleaseDate = releaseDate;
+        this.IsbnNumber = isbnNumber;
+        this.Price = price;
+    }
+
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string Publisher { get; set; }
+    public DateTime ReleaseDate { get; set; }
+    public string IsbnNumber { get; set; }
+    public decimal Price { get; set; }
+}
+public class Library
+{
+    public Library(string name)
+    {
+        this.Name = name;
+        this.Books = new List<Book>();
+    }
+    public string Name { get; set; }
+    public List<Book> Books { get; set; }
+}
---
+++ 07.FilesDirectoriesAndExceptions - Exercises/BookLibraryModification/BookLibraryModification.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.IO;
+
+public class BookLibraryModification
+{
+    public static void Main()
+    {
+        string[] input = File.ReadAllLines("input.txt");
+        int bookCounter = int.Parse(input[0]);
+
+        Library library = new Library("My New Library");
+
+        for (int i = 0; i < bookCounter; i++)
+        {
+            string inputLine = input[i + 1];
+            string[] inputArgs = inputLine.Split(' ');
+            string title = inputArgs[0];
+            string author = inputArgs[1];
+            string publisher = inputArgs[2];
+            DateTime releaseDate = DateTime.ParseExact(inputArgs[3], "dd.MM.yyyy", CultureInfo.InvariantCulture);
+            string IsbnNumber = inputArgs[4];
+            decimal price = decimal.Parse(inputArgs[5]);
+
+            Book book = new Book(title, author, publisher, releaseDate, IsbnNumber, price);
+            library.Books.Add(book);
+        }
+
+        DateTime sortedDate = DateTime.ParseExact(input[6], "dd.MM.yyyy", CultureInfo.InvariantCulture);
+
+        Dictionary<string, DateTime> booksByAuthors = new Dictionary<string, DateTime>();
+
+        foreach (Book book in library.Books)
+        {
+            if (!booksByAuthors.ContainsKey(book.Title))
+            {
+                booksByAuthors.Add(book.Title, book.ReleaseDate);
+            }
+            else
+            {
+                booksByAuthors[book.Title] = book.ReleaseDate;
+            }
+        }
+
+        foreach (var book in booksByAuthors.Where(x => x.Value > sortedDate).OrderBy(y => y.Value).ThenBy(s=>s.Key))
+        {
+            File.AppendAllText("output.txt", string.Format($"{book.Key} -> {book.Value.ToString("dd.MM.yyyy")}" + Environment.NewLine));
+        }
+    }
+}
+
+public class Book
+{
+    public Book(string title, string author, string publisher, DateTime releaseDate, string IsbnNumber, decimal price)
+    {
+        this.Title = title;
+        this.Author = author;
+        this.Publisher = publisher;
+        this.ReleaseDate = releaseDate;
+        this.ISBNnumber = IsbnNumber;
+        this.Price = price;
+    }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string Publisher { get; set; }
+    public DateTime ReleaseDate { get; set; }
+    public string ISBNnumber { get; set; }
+    public decimal Price { get; set; }
+}
+
+public class Library
+{
+    public Library(string name)
+    {
+        this.Name = name;
+        this.Books = new List<Book>();
+    }
+
+    public string Name { get; set; }
+    public List<Book> Books { get; set; }
+}
